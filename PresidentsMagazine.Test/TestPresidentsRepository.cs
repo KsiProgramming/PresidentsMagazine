587a58a265608035686132ea720bb7b9a7a9d812
Ksi <labani.said@outlook.com>
2023-02-04 09:11:57 +0100

    Version 2 was for demo purposes

--- PresidentsMagazine.Test/TestPresidentsRepository.cs
+++ PresidentsMagazine.Test/TestPresidentsRepository.cs
@@ -2,15 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using PresidentsMagazine.Data;
 using PresidentsMagazine.Repositories.PresidentsRepository;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.Metrics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PresidentsMagazine.Test
 {
@@ -31,37 +24,12 @@ namespace PresidentsMagazine.Test
                                                     .AddScoped<IPresidentsRepository, PresidentsRepository>()
                                                     .BuildServiceProvider();
 
-            var presidentsRepository = services.GetRequiredService<IPresidentsRepository>();            
+            var presidentsRepository = services.GetRequiredService<IPresidentsRepository>();
             //Assert
             await Assert.ThrowsAsync<ArgumentException>( //Act
                                                         () => presidentsRepository.GetByIdAsync(Guid.Empty)
                                                         );
         }
-        [Fact]
-        public async Task GetPresidentByIdAsync_EmptyGuid_ThrowExceptionV2()
-        {
-            //Arrange
-            var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = ":memory:" };
-            var connection = new SqliteConnection(connectionStringBuilder.ConnectionString);
-            var options = new DbContextOptionsBuilder<PresidentsMagazineContext>().UseSqlite(connection).Options;
-            using (var context = new PresidentsMagazineContext(options))
-            {
-                //context.Database.OpenConnection();
-                //context.Database.EnsureCreated();               
-                //context.SaveChanges();
-            }
-            using var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
-            var logger = loggerFactory.CreateLogger<PresidentsRepository>();
-            
-            using (var context = new PresidentsMagazineContext(options))
-            {
-                var presidentsRepository = new PresidentsRepository(logger, context);
-
-                //Assert
-                await Assert.ThrowsAsync<ArgumentException>( //Act
-                                                            () => presidentsRepository.GetByIdAsync(Guid.Empty)
-                                                            );
-            }
-        }
+       
     }
 }
